aec3340cfefcaacb8e131ea313f5c8cd11cb5333
Germana <gmsvaz@gmail.com>
2022-11-09 01:02:28 -0300

    padrões gof singleton e strategy

---
+++ lab-padroes-projeto-java/src/dio/gof/strategy/Teste.java
@@ -0,0 +1,29 @@
+package dio.gof.strategy;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		// Testes relacionados ao Design Pattern Strategy
+		
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.mover();
+		
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+
+	}
+
+}
