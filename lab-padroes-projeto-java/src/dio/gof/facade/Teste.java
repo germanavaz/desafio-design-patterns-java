10559933484503299446f9a57d3b3a19a1d549a6
Germana <gmsvaz@gmail.com>
2022-11-09 01:23:09 -0300

    padrão facade

---
+++ lab-padroes-projeto-java/src/dio/gof/facade/Teste.java
@@ -0,0 +1,13 @@
+package dio.gof.facade;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// Teste relacionado ao Design Pattern Facade 
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Venilton", "12345678");
+
+	}
+
+}
